3f6f35dc0851d4f55313f4beac04a06e1b716a82
Marta <marta.prazanowska@hotmail.com>
2019-03-04 23:37:03 +0100

    Karatsuba multiplication added
    Basic implementation of Karatsuba multiplication algorithm.

---
+++ AlgorithmsIlluminated/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlgorithmsIlluminated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication using Karatsuba multiplication algorithm");
+            Console.WriteLine("Please enter two comma separated numbers:");
+
+            var numbers = Console.ReadLine().Split(",");
+
+            try
+            {
+                var result = BasicAlgorithmsHelper.CalculateKaratsubaMultiplication(numbers[0], numbers[1]);
+                Console.WriteLine($"Result is {result}.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Exception ccurred: {ex.Message}.");
+            }
+            finally
+            {
+                Console.WriteLine($"Press any key to continue.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
